f8bcfd75f9902be22987ed3c4d891778eceb9a97
SharksevenRo <937257166@qq.com>
2015-09-28 16:58:51 +0800

    添加Java-微信-tools依赖

---
+++ src/main/java/com/ncshop/controlls/NcWxMpServlet.java
@@ -0,0 +1,112 @@
+package com.ncshop.controlls;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.common.session.WxSessionManager;
+import me.chanjar.weixin.common.util.StringUtils;
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpMessageHandler;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.api.WxMpServiceImpl;
+import me.chanjar.weixin.mp.bean.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.WxMpXmlOutMessage;
+import me.chanjar.weixin.mp.bean.WxMpXmlOutTextMessage;
+
+public class NcWxMpServlet extends HttpServlet {
+
+	protected WxMpInMemoryConfigStorage wxMpConfigStorage;
+	protected WxMpService wxMpService;
+	protected WxMpMessageRouter wxMpMessageRouter;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		wxMpConfigStorage = new WxMpInMemoryConfigStorage();
+		wxMpConfigStorage.setAppId("wxd8276cabf8323d91"); // 设置微信公众号的appid
+		wxMpConfigStorage.setSecret("64f28217f2ea488418026fac44506e4b"); // 设置微信公众号的app corpSecret
+		wxMpConfigStorage.setToken("ncshops"); // 设置微信公众号的token
+		wxMpConfigStorage.setAesKey("ncshops"); // 设置微信公众号的EncodingAESKey
+
+		wxMpService = new WxMpServiceImpl();
+		wxMpService.setWxMpConfigStorage(wxMpConfigStorage);
+
+		WxMpMessageHandler handler = new WxMpMessageHandler() {
+			@Override
+			public WxMpXmlOutMessage handle(WxMpXmlMessage wxMessage,
+					Map<String, Object> context, WxMpService wxMpService,
+					WxSessionManager sessionManager) throws WxErrorException {
+				WxMpXmlOutTextMessage m = WxMpXmlOutMessage.TEXT()
+						.content("测试加密消息").fromUser(wxMessage.getToUserName())
+						.toUser(wxMessage.getFromUserName()).build();
+				return m;
+			}
+		};
+
+		wxMpMessageRouter = new WxMpMessageRouter(wxMpService);
+		wxMpMessageRouter.rule().async(false).content("哈哈") // 拦截内容为“哈哈”的消息
+				.handler(handler).end();
+
+	}
+
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		response.setStatus(HttpServletResponse.SC_OK);
+
+		String signature = request.getParameter("signature");
+		String nonce = request.getParameter("nonce");
+		String timestamp = request.getParameter("timestamp");
+
+		if (!wxMpService.checkSignature(timestamp, nonce, signature)) {
+			// 消息签名不正确，说明不是公众平台发过来的消息
+			response.getWriter().println("非法请求");
+			return;
+		}
+
+		String echostr = request.getParameter("echostr");
+		if (StringUtils.isNotBlank(echostr)) {
+			// 说明是一个仅仅用来验证的请求，回显echostr
+			response.getWriter().println(echostr);
+			return;
+		}
+
+		String encryptType = StringUtils.isBlank(request
+				.getParameter("encrypt_type")) ? "raw" : request
+				.getParameter("encrypt_type");
+
+		if ("raw".equals(encryptType)) {
+			// 明文传输的消息
+			WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(request
+					.getInputStream());
+			WxMpXmlOutMessage outMessage = wxMpMessageRouter.route(inMessage);
+			response.getWriter().write(outMessage.toXml());
+			return;
+		}
+
+		if ("aes".equals(encryptType)) {
+			// 是aes加密的消息
+			String msgSignature = request.getParameter("msg_signature");
+			WxMpXmlMessage inMessage = WxMpXmlMessage.fromEncryptedXml(
+					request.getInputStream(), wxMpConfigStorage, timestamp,
+					nonce, msgSignature);
+			WxMpXmlOutMessage outMessage = wxMpMessageRouter.route(inMessage);
+			response.getWriter().write(
+					outMessage.toEncryptedXml(wxMpConfigStorage));
+			return;
+		}
+		response.getWriter().println("不可识别的加密类型");
+		return;
+	}
+
+}
