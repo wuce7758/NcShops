559c4ea0740efb761685be675764165bcb52b953
549781430 <549781430@qq.com>
2015-10-02 22:38:13 +0800

    xx

--- src/main/java/com/ncshop/service/SellerService.java
+++ src/main/java/com/ncshop/service/SellerService.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ncshop.dao.TGoodsDAO;
 import com.ncshop.dao.TGoodstypeDAO;
+import com.ncshop.dao.TOrderDAO;
 import com.ncshop.dao.TSellerDAO;
 import com.ncshop.dao.TSellergoodsDAO;
 import com.ncshop.dao.TUserDAO;
@@ -14,6 +16,7 @@ import com.ncshop.domain.TGoodstype;
 import com.ncshop.domain.TOrder;
 import com.ncshop.domain.TSeller;
 import com.ncshop.domain.TSellergoods;
+import com.ncshop.domain.TUser;
 
 @Service
 public class SellerService {
@@ -27,6 +30,10 @@ public class SellerService {
 	private TGoodstypeDAO goodstypeDao;
 	@Autowired
 	private TGoodstypeDAO goodtypeDao;
+	@Autowired
+	private TGoodsDAO goodsDao;
+	@Autowired
+	private TOrderDAO orderDao;
 
 	public List<TOrder> findSellerOrder(int openId, int orderState) {
 		// TODO Auto-generated method stub
@@ -41,7 +48,7 @@ public class SellerService {
 		goods.setTGoodstype(goodsType);
 		sellergoods.setTGoods(goods);
 		sellergoods.setTSeller(seller);
-		
+		goodsDao.save(goods);
 		sellergoodsDao.save(sellergoods);
 	}
 
@@ -52,6 +59,32 @@ public class SellerService {
 
 	public void limitUser(int userId) {
 		// TODO Auto-generated method stub
+		TUser user=userDao.findById(userId);
+		if(user.getUserState()==true){
+			user.setUserState(false);
+		}else{
+			user.setUserState(true);
+		}
 		
+	}
+	//修改商品信息
+	public void updateGoods(int goodsTypeId, TGoods goods) {
+		// TODO Auto-generated method stub
+		TGoodstype goodsType=goodstypeDao.findById(goodsTypeId);
+		TGoods gs=goodsDao.findById(goods.getGoodsId());
+		gs.setGoodsMsg(goods.getGoodsMsg());
+		gs.setGoodsName(goods.getGoodsName());
+		gs.setGoodsPic(goods.getGoodsPic());
+		gs.setGoodsPrice(goods.getGoodsPrice());
+		gs.setTGoodstype(goodsType);
+		goodsDao.save(gs);
+	}
+
+	public TOrder changeOrderState(int orderId, int orderState) {
+		// TODO Auto-generated method stub
+		TOrder order=orderDao.findById(orderId);
+		order.setOrderState(orderState);
+		orderDao.save(order);
+		return order;
 	}
 }
