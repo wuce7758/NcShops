2ce49c9123c5d8466cc97efaee10e3acf3ebcf78
549781430 <549781430@qq.com>
2015-10-02 22:38:32 +0800

    xx

--- src/main/java/com/ncshop/controlls/SellerController.java
+++ src/main/java/com/ncshop/controlls/SellerController.java
@@ -1,24 +1,18 @@
 package com.ncshop.controlls;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.multipart.MultipartFile;
 import com.ncshop.domain.TGoods;
 import com.ncshop.domain.TGoodstype;
 import com.ncshop.domain.TOrder;
-import com.ncshop.domain.TSeller;
-import com.ncshop.domain.TSellergoods;
-import com.ncshop.domain.TUser;
 import com.ncshop.service.SellerService;
 import com.ncshop.service.UserService;
 
@@ -49,20 +43,101 @@ public class SellerController{
 		response.getWriter().write("添加商品成功!");
 	}
 	/**
+	 * 更改订单状态，比如订单完成
+	 * @param response
+	 * @param orderId
+	 * @param orderState
+	 * @throws IOException 
+	 */
+	@RequestMapping("/changeOrderState")
+	public void changeOrderState(HttpServletResponse response,int orderId,int orderState) throws IOException{
+		if(orderId+""==""||orderState+""==""){
+			return;
+		}
+		TOrder order=sellerService.changeOrderState(orderId,orderState);
+		if(order!=null){
+			response.setContentType("html/text;charset=utf-8");
+			response.getWriter().write("添加商品成功!");			
+		}
+	}
+	/**
 	 * 添加店铺商品
 	 * @param sellerId 卖家唯一标识
 	 * @param goods 新商品
 	 * @throws Exception
 	 */
 	@RequestMapping("/addGoods")
-	public void addGoods(HttpServletResponse response,int sellerId,int goodsTypeId,TGoods goods) throws Exception{	
+	public void addGoods(HttpServletResponse response,
+			int sellerId,int goodsTypeId,
+			MultipartFile picFile, TGoods goods) throws Exception{	
 		if(sellerId+""==""||goodsTypeId+""==""){
 			return;
 		}
-		//调用service查找 数据库
+		//定义物理路径
+		String basePath="C:\\fileupload\\goodspic\\"+goodsTypeId+"\\";
+		File f=new File(basePath);
+		if(!f.exists()) {
+			f.mkdirs();
+		}
+		if(picFile!=null){
+			//旧文件名
+			String fileName=picFile.getOriginalFilename();
+			//新文件名
+			String newFileName=new Date().getTime()+fileName.substring(fileName.lastIndexOf("."));
+			File file=new File(basePath+newFileName);
+			//将文件保存到硬盘
+			picFile.transferTo(file);
+			//重新设置图片名
+			goods.setGoodsPic(newFileName);
+			//调用service查找 数据库
+		}else{
+			//设置默认图片
+			goods.setGoodsPic("default.jpg");
+		}
 		sellerService.addGoods(sellerId,goodsTypeId,goods);
 		response.setContentType("text/html;charset=utf-8");
 		response.getWriter().write("添加商品成功!");
+	}
+	/**
+	 * 修改商品信息
+	 * @param response
+	 * @param sellerId
+	 * @param goodsTypeId
+	 * @param picFile
+	 * @param goods
+	 * @throws Exception
+	 */
+	@RequestMapping("/updateGoods")
+	public void updateGoods(HttpServletResponse response,
+			int goodsTypeId,
+			MultipartFile picFile, TGoods goods) throws Exception{	
+		if(goodsTypeId+""==""){
+			return;
+		}
+		//定义物理路径
+		String basePath="C:\\fileupload\\goodspic\\"+goodsTypeId+"\\";
+		File f=new File(basePath);
+		if(!f.exists()) {
+			f.mkdirs();
+		}
+		if(picFile!=null){
+			//旧文件名
+			String fileName=picFile.getOriginalFilename();
+			//新文件名
+			String newFileName=new Date().getTime()+fileName.substring(fileName.lastIndexOf("."));
+			File file=new File(basePath+newFileName);
+			//将文件保存到硬盘
+			picFile.transferTo(file);
+			//重新设置图片名
+			goods.setGoodsPic(newFileName);
+			//调用service查找 数据库
+		}else{
+			//设置默认图片
+			goods.setGoodsPic("default.jpg");
+		}
+		sellerService.updateGoods(goodsTypeId,goods);
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().write("商品修改成功!");
 	}
 	/**
 	 * 添加商品类型
@@ -82,7 +157,7 @@ public class SellerController{
 	}
 	/**
 	 * 拉黑用户
-	 * @param goodsType 商品类型对象
+	 * @param userId 
 	 * @throws Exception
 	 */
 	@RequestMapping("/limitUser")
