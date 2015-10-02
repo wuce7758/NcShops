5d2b00e1a3d1083a34b840fea959b7580a5c8ffc
549781430 <549781430@qq.com>
2015-10-02 09:16:28 +0800

    xxx

--- src/main/java/com/ncshop/controlls/SellerController.java
+++ src/main/java/com/ncshop/controlls/SellerController.java
@@ -3,6 +3,9 @@ package com.ncshop.controlls;
 import java.util.List;
 import java.util.Set;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ncshop.domain.TGoods;
+import com.ncshop.domain.TGoodstype;
 import com.ncshop.domain.TOrder;
 import com.ncshop.domain.TSeller;
 import com.ncshop.domain.TSellergoods;
@@ -20,6 +24,7 @@ import com.ncshop.service.UserService;
 
 
 @Controller
+@RequestMapping("/seller")
 public class SellerController{
 	
 	@Autowired
@@ -33,19 +38,46 @@ public class SellerController{
 	 * @param orderState 订单状态
 	 * @throws Exception
 	 */
-	@RequestMapping("/findOrders")
-	public @ResponseBody List<TOrder> findOrders(int OpenId,int orderState) throws Exception{	
+	@RequestMapping("/findSellerOrders")
+	public void findSellerOrders(HttpServletResponse response,int OpenId,int orderState) throws Exception{	
+		if(OpenId+""==""||orderState+""==""){
+			return;
+		}
 		//调用service查找 数据库
 		List<TOrder> orderList=sellerService.findSellerOrder(OpenId,orderState);
-		return orderList;
+		response.setContentType("html/text;charset=utf-8");
+		response.getWriter().write("添加商品成功!");
 	}
 	/**
 	 * 添加店铺商品
+	 * @param sellerId 卖家唯一标识
+	 * @param goods 新商品
 	 * @throws Exception
 	 */
 	@RequestMapping("/addGoods")
-	public void addGoods(@RequestBody TSellergoods sellergoods) throws Exception{	
+	public void addGoods(HttpServletResponse response,int sellerId,int goodsTypeId,TGoods goods) throws Exception{	
+		if(sellerId+""==""||goodsTypeId+""==""){
+			return;
+		}
 		//调用service查找 数据库
-		int userList=sellerService.addGoods(sellergoods);
+		sellerService.addGoods(sellerId,goodsTypeId,goods);
+		response.setContentType("text/html;charset=utf-8");
+		response.getWriter().write("添加商品成功!");
+	}
+	/**
+	 * 添加商品类型
+	 * @param goodsType 商品类型对象
+	 * @throws Exception
+	 */
+	@RequestMapping("/addGoodsType")
+	public void addGoodsType(HttpServletResponse response,TGoodstype goodsType) throws Exception{	
+		String goodsTypeName=goodsType.getGoodsTypeName();
+		if(goodsTypeName==null||goodsTypeName==""){
+			return;
+		}
+		//调用service查找 数据库
+		sellerService.addGoodsType(goodsType);
+		response.setContentType("html/text;charset=utf-8");
+		response.getWriter().write("添加商品类型成功!");
 	}
 }
