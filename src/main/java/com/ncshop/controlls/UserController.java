0093916d9404e09d80edfd2e966d9da69cd4e332
SharksevenRo <937257166@qq.com>
2015-10-15 23:49:32 +0800

    get

--- src/main/java/com/ncshop/controlls/UserController.java
+++ src/main/java/com/ncshop/controlls/UserController.java
@@ -1,5 +1,6 @@
 package com.ncshop.controlls;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -220,7 +221,7 @@ public class UserController {
 			}
 			request.getSession().setAttribute("odersdetails", orderdetails);
 
-			request.setAttribute("allCost", allCost);
+			request.getSession().setAttribute("allCost", allCost);
 			TUser user = (TUser) request.getSession().getAttribute("user");
 			List<TAddress> address = null;
 			if (user != null) {
@@ -308,20 +309,28 @@ public class UserController {
 			HttpServletRequest request, HttpServletResponse response) {
 		try {
 			TUser user = (TUser) request.getSession().getAttribute("user");
-			if (user.getUserId() != null) {
-				if (userService.updateAddress(user.getUserId(), address)) {
-					List<TAddress> findAddress = userService.findAddress(2);
-					request.setAttribute("address", findAddress);
-					// 默认地址修改成功，跳转
-					request.getRequestDispatcher("/custom/MyOrder.jsp")
-							.forward(request, response);
+			if (user != null) {
+				if (user.getUserId() != null) {
+					if (userService.updateAddress(user.getUserId(), address)) {
+						List<TAddress> findAddress = userService.findAddress(2);
+						request.setAttribute("address", findAddress);
+						// 默认地址修改成功，跳转
+					}
+				} else {
+					address.setIsDefault(true);
+					userService.bind(user, address);
 				}
-			} else {
+			}else{
 				address.setIsDefault(true);
-				userService.bind(user, address);
-				request.getRequestDispatcher("/custom/MyOrder.jsp").forward(
-						request, response);
+				userService.bind(new TUser(), address);
+				
+				
 			}
+			List<TAddress> addresses=new ArrayList<TAddress>();
+			addresses.add(address);
+			request.setAttribute("address", addresses);
+			request.getRequestDispatcher("/custom/MyOrder.jsp")
+			.forward(request, response);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
