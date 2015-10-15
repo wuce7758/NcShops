73327bfe4ecb8413a6ffe0219333935d5b2b189b
549781430 <549781430@qq.com>
2015-10-15 11:50:52 +0800

    dd

--- src/main/java/com/ncshop/controlls/SellerController.java
+++ src/main/java/com/ncshop/controlls/SellerController.java
@@ -1,7 +1,9 @@
 package com.ncshop.controlls;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 
@@ -11,12 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.ncshop.domain.TGoods;
 import com.ncshop.domain.TGoodstype;
 import com.ncshop.domain.TOrder;
 import com.ncshop.domain.TSeller;
 import com.ncshop.service.SellerService;
 import com.ncshop.service.UserService;
+import com.ncshop.util.TargetStrategy;
 
 @Controller
 @RequestMapping("/seller")
@@ -58,8 +64,9 @@ public class SellerController {
 		// 调用service查找 数据库
 		List<TOrder> orderList = sellerService.findSellerOrder(OpenId,
 				orderState);
+		String json=toJson(new TOrder(),orderList,null);
 		response.setContentType("html/text;charset=utf-8");
-		response.getWriter().write("添加商品成功!");
+		response.getWriter().write(json);
 	}
 
 	/**
@@ -110,7 +117,6 @@ public class SellerController {
 
 	/**
 	 * 添加商品类型
-	 * 
 	 * @param goodsType商品类型对象
 	 * @throws Exception
 	 */
@@ -125,5 +131,44 @@ public class SellerController {
 		sellerService.addGoodsType(goodsType);
 		response.setContentType("html/text;charset=utf-8");
 		response.getWriter().write("添加商品类型成功!");
+	}
+	
+	/**
+	 * 获取所有商品类型
+	 * @throws Exception
+	 */
+	@RequestMapping("/getGoodsType")
+	public void getAllGoodsType(HttpServletResponse response) throws Exception {
+		// 调用service查找 数据库
+		List<TGoodstype> list=sellerService.getAllGoodsType();
+		String json=toJson(new TGoodstype(),list,null);
+		response.setContentType("html/text;charset=utf-8");
+		response.getWriter().write(json);
+	}
+	
+	/**
+	 * 将集合转换成json
+	 * @param t
+	 * @param list
+	 * @param fieldNames
+	 * @param flag
+	 * @return
+	 */
+	private <T> String toJson(T t, List<T> list, String[] fieldNames) {
+		Gson gson = null;
+		Map<String, List<T>> map = new HashMap<String, List<T>>();
+		map.put(t.getClass().getName().replace("com.ncshop.domain.", ""), list);
+		if (fieldNames != null) {
+			TargetStrategy ts = null;
+			ts = new TargetStrategy(t.getClass());
+			ts.setReverse(true);
+			ts.setFields(fieldNames);
+			gson = new GsonBuilder().setExclusionStrategies(ts).create();
+		} else {
+			gson = new Gson();
+		}
+		String json = gson.toJson(map);
+		System.out.println(json);
+		return json;
 	}
 }
